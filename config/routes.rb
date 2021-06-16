f0b4d688449804c0f6f024b24452225f20e143b8
Ryan Searls <Ryan.searls@gmail.com>
2021-06-16 13:38:37 -0500

    added routes to products

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get "/all_products", controller: "products", action: "all_products"
+
+  get "/first_product", controller: "products", action: "Titleist_ap2"
+
+  get "/second_product", controller: "products", action: "Scotty_cameron"
+
+  get "/third_product", controller: "products", action: "Taylormade_m4"
+
+  
+  
+
 end
