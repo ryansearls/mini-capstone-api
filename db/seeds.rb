f9a346155c6c97306d0ba191db71b0714bb2ae66
Ryan Searls <Ryan.searls@gmail.com>
2021-06-16 13:50:21 -0500

    added images to products

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  product1 = Product.create(name: "Titleist_ap2", price: 1000, image_url: "", description: "Top of the line brand new gold irons.")
+  product1 = Product.create(name: "Titleist_ap2", price: 1000, image_url: "https://cdn.golfmagic.com/styles/scale_768/s3/47169.jpg", description: "Top of the line golf irons.")
 
-  product2 = Product.create(name: "Scotty_cameron", price: 400, image_url: "", description: "Putt like the pros!")
+  product2 = Product.create(name: "Scotty_cameron", price: 400, image_url: "https://www.scottycameron.com/media/12383/18select_familylanding_newport.jpg", description: "Putt like the pros!")
 
-  product3 = Product.create(name: "Taylormade_m4", price: 5000, image_url: "", description: "Hit the ball farther and straighter.")
+  product3 = Product.create(name: "Taylormade_m4", price: 5000, image_url: "https://image.globalgolf.com/dynamic/1042862/aci/sole-view/taylormade-m4-2018-driver.jpg?s=350", description: "Hit the ball further and straighter.")
