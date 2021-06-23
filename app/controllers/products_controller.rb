5e312b24bc527c59544838acd94278dc5290a6f3
Ryan Searls <Ryan.searls@gmail.com>
2021-06-23 12:40:24 -0500

    validations and happy/sad paths

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -13,8 +13,14 @@ class ProductsController < ApplicationController
       image_url: params[:image_url],
       description: params[:description])
 
-    product.save
+    
+    if product.save
     render json: product
+    else
+      render json: {errors: product.errors.full_messages},
+      status: : 418
+    end   
+
   end 
 
   def show
@@ -31,9 +37,14 @@ class ProductsController < ApplicationController
     product.price = params[:price] || product.price
     product.image_url = params[:image_url] || product.image_url
     product.description = params[:description] || product.description
-    product.save
+    
+    if product.save
     render json: product.as_json
-  end   
+    else
+      render json: {errors: product.errors.full_messages},
+      status: :unprocessable_entity
+    end
+  end
   
 
   def destroy
