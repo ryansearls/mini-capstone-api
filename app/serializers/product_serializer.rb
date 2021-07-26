ccef3cdb0feb80580e5b39270d4cd0a835e22230
Ryan Searls <Ryan.searls@gmail.com>
2021-07-26 13:56:28 -0500

    updated db

--- app/serializers/product_serializer.rb
+++ app/serializers/product_serializer.rb
@@ -1,5 +1,5 @@
 class ProductSerializer < ActiveModel::Serializer
-  attributes :id, :name, :price, :image_url, :description, :is_discounted?, :tax, :total, 
+  attributes :id, :name, :price, :image_url, :description, :is_discounted?, :tax, :total
 
   belongs_to :supplier
   has_many :categories
